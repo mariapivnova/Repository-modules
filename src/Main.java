4a95681060aa6c891668546633bbf961cb50718c
maria <mariapivnova@yandex.ru>
2022-02-16 14:44:29 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         for (int i=0;i<10;i++){
-            for (int j=0;j<10;j++){
-                System.out.printf("%5d",(i+1)*(j+1));
+            for (int x=0;x<10;x++){
+                System.out.printf("%6d",(i+1)*(x+1));
             }
         }
     }
