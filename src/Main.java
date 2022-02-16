e4a265bc6cd040ed2c8f86058f80cb8951b64151
maria <mariapivnova@yandex.ru>
2022-02-16 15:34:24 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i=0;i<10;i++){
             for (int x=0;x<10;x++){
-                System.out.printf("%6d",(i+1)*(x+1));
+                System.out.println( (i+1)+"*"+(x+1)+"="+(i+1)*(x+1));
             }
         }
     }
