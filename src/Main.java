98f9608537337cbc83226af92678f5e1596a08bc
maria <mariapivnova@yandex.ru>
2022-02-16 14:40:17 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i=0;i<10;i++){
             for (int j=0;j<10;j++){
-                System.out.printf("%5d",(i+1)*(j+1));
+                System.out.printf("%4d",(i+1)*(j+1));
             }
         }
     }
